bda788baed83bfcc03f12a6e0d246ad5bdf7c542
William Sackfield <will.sackfield@gmail.com>
2022-03-13 22:44:29 -0400

    Fix parsing Tor2Web requests

--- tormiddleware/middleware.py
+++ tormiddleware/middleware.py
@@ -61,22 +61,21 @@ class TORDownloaderMiddleware:
         #   installed downloader middleware will be called
         if self.should_process_request(request, spider):
             tor2web_proxy = request.meta.get("tor2web_proxy", spider.settings.get("TOR2WEB_PROXY", None))
-            if tor2web_proxy is not None and self.should_process_request(request.url):
+            if tor2web_proxy is not None and self.should_process_url(request.url):
                 proxy_parse = urllib.parse.urlparse(tor2web_proxy)
                 parse = urllib.parse.urlparse(request.url)
                 extracted = tldextract.extract(request.url)
                 proxy_extracted = tldextract.extract(tor2web_proxy)
-                request.url = urllib.parse.urlunparse(
+                return request.replace(url=urllib.parse.urlunparse(
                     (
                         proxy_parse[0],
-                        ".".join([extracted.subdomain, proxy_extracted.domain, proxy_extracted.suffix]),
+                        ".".join([extracted.domain, proxy_extracted.domain, proxy_extracted.suffix]),
                         parse[2],
                         parse[3],
                         parse[4],
                         parse[5],
                     )
-                )
-                return request
+                ))
             return self.perform_tor_request(request)
         return None
 
